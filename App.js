5992ae0dc2dd3c638d038ec6086e47226f80180f
Victor Rosas <victor.rosas.96@hotmail.com>
2022-05-11 22:16:00 -0500

    estilizar registrar e buscar tarefas

--- App.js
+++ App.js
@@ -1,10 +1,82 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+   View,
+    Text, 
+    StyleSheet, 
+    SafeAreaView, 
+    TextInput, 
+    TouchableOpacity, 
+    FlatList,
+    Keyboard
+} from 'react-native';
 
 import Login from './src/components/Login';
+import TaskList from './src/components/TaskList';
+
+import firebase from './src/services/firebaseConnection';
 
 export default function App() {
   const [user, setUser] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+
+    function getUser() {
+      if(!user) {
+        return;
+      }
+
+      firebase.database().ref('tarefas').child(user).once('value', (snapshot) => {
+        setTasks([]);
+
+        snapshot?.forEach((childItem) => {
+          let data = {
+            key: childItem.key,
+            nome: childItem.val().nome
+          }
+
+          setTasks(oldTaks => [...oldTaks, data])
+        })
+      })
+    }
+
+    getUser();
+
+  }, [user]);
+
+  function handleAdd() {
+    if(newTask === '') {
+      return;
+    }
+
+    let tarefas = firebase.database().ref('tarefas').child(user);
+    let chave = tarefas.push().key;
+
+    tarefas.child(chave).set({
+      nome: newTask
+    })
+    .then(() => {
+      const data = {
+        key: chave,
+        nome: newTask
+      };
+
+      setTasks(oldTaks => [...oldTaks, data])
+    });
+
+    Keyboard.dismiss();
+    setNewTask('');
+  }
+
+  function handleDelete(key) {
+    console.log(key);
+  }
+
+  function handleEdit(data) {
+    console.log("ITEM CLICADO", data);
+  }
 
   if(!user) {
     return <Login changeStatus={(user) => setUser(user)}/>
@@ -12,7 +84,26 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Dentro da tela de Tarefas</Text>
+     
+    <View style={styles.containerTask}>
+      <TextInput
+      style={styles.input}
+      placeholder="O que vai fazer hoje?"
+      value={newTask}
+      onChangeText={ (text) => setNewTask(text)}
+      /> 
+      <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+
+    <FlatList 
+    data={tasks}
+    keyExtractor={ item => item.key}
+    renderItem={ ({ item }) => (
+      <TaskList data={item} deleteItem={handleDelete} editItem={handleEdit} />
+    )}
+    />
     </SafeAreaView>
   );
 };
@@ -20,8 +111,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 25,
+    paddingTop: 35,
     paddingHorizontal: 10,
     backgroundColor: '#f2f6fc'
+  },
+  containerTask: {
+    flexDirection: 'row',
+    marginLeft: 5,
+  },
+  input: {
+    flex: 1,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#141414',
+    height: 45
+  },
+  buttonAdd: {
+    backgroundColor: '#141414',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+    paddingHorizontal: 14,
+    borderRadius: 4
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 22
   }
 });
